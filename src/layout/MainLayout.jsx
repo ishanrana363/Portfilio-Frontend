1cec18b234b77f295dc702cf7b1c55d1bd523fd8
ishanrana363 <ishanrana363@gmail.com>
2024-10-02 23:05:23 +0600

    header footer react router added

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+const MainLayout = () => {
+    return (
+        <div>
+
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default MainLayout
