f194210a15b9e970048f64eb20e31ae0c7370885
ishanrana363 <ishanrana363@gmail.com>
2024-10-09 19:55:44 +0600

    vercel sjon file added

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -13,26 +13,25 @@ const Skill = () => {
 
 
   return (
-    <div className='bg-bgPrimary min-h-screen text-white'>
+    <div className='bg-bgPrimary min-h-screen text-white  '>
       <div className='max-w-screen-xl mx-auto py-10'>
         <div>
-          <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6'>
+          <div className='grid lg:grid-cols-4 md:grid-cols-3  sm:grid-cols-2 gap-6'>
             {allSkillData &&
               allSkillData.map((item, i) => {
                 return (
                   <div key={i} className="h-full">
-                    <div className="card bg-gray-400 shadow-xl h-full transform transition duration-500 hover:scale-75 hover:shadow-2xl">
+                    <div className="card bg-gray-400 shadow-xl transform transition duration-500 hover:scale-75 hover:shadow-2xl">
                       <figure className="px-10 pt-10">
                         <img
                           src={item?.img}
-                          className="rounded-full hover:scale-125 hover:translate-x-6 hover:duration-1000 border border-bgPrimary"
+                          className="rounded-full hover:scale-125 hover:translate-x-2 hover:duration-1000 border border-bgPrimary"
                           alt="Skill Image"
                           style={{ width: '200px', height: '200px', objectFit: 'cover' }}
                         />
                       </figure>
                       <div className="card-body items-center text-center">
-                        <h2 className="card-title">{item?.name}</h2>
-                        <p>{item?.description}</p>
+                        <h2 className="card-title font-serif ">{item?.name}</h2>
                       </div>
                     </div>
                   </div>
