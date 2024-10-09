99333259a207b2135bf657b95c165f6405823edf
ishanrana363 <ishanrana363@gmail.com>
2024-10-09 19:17:02 +0600

    animation addeed

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,26 @@ export default {
         pSmallDevice : "19px",
         h1SmallDevice : "32px",
         h2SmallDevice : "28px",
-      }
+      },
+      animation: {
+        'zoom-in': 'zoomIn 0.6s ease-in-out forwards',
+        'fade-in-left': 'fadeInLeft 0.6s ease-in-out forwards',
+        'fade-in-right': 'fadeInRight 0.6s ease-in-out forwards',
+      },
+      keyframes: {
+        zoomIn: {
+          '0%': { transform: 'scale(0.8)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-50px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(50px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
     },
   },
   plugins: [require('daisyui')],
