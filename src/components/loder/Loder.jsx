8a464a2bed7805dc1c2dd92fa12e3789c9caa3f7
ishanrana363 <ishanrana363@gmail.com>
2024-10-09 20:55:05 +0600

    addd loder

---
+++ src/components/loder/Loder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="border-t-4 border-b-4 text-fuchsia-600 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
