f17a62049b4453164e5301e15e36e55730fca6ae
ishanrana363 <ishanrana363@gmail.com>
2024-10-12 22:49:34 +0600

    navbar modify

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,22 @@
-
-import { NavLink, useLocation } from 'react-router-dom'
+import { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const { pathname } = useLocation();
-  console.log(pathname);
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   return (
-    <div className='bg-navFooterGray shadow-md shadow-green-200 font-mono font-bold text-2lg text-textSecondary sticky top-0 z-10 '>
+    <div className='bg-navFooterGray shadow-md shadow-green-200 font-mono font-bold text-2xl text-textSecondary sticky top-0 z-10'>
       <div className="navbar max-w-screen-xl mx-auto">
         {/* Navbar Start */}
         <div className="navbar-start">
           {/* Toggle Button for Small Screens */}
           <div className="dropdown md:hidden">
-            <label tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <label onClick={handleToggle} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -28,7 +32,7 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              className={`menu menu-sm dropdown-content bg-bgPrimary mt-3 z-[1] p-2 shadow rounded-box w-52 ${toggle ? "block duration-700 " : "hidden duration-700 "}`}>
               <li><NavLink to={"/"}>Home</NavLink></li>
               <li><NavLink to={"/about"}>About</NavLink></li>
               <li><NavLink to={"/skill"}>Skills</NavLink></li>
@@ -47,7 +51,7 @@ const Header = () => {
         {/* Navbar Center for Large Screens */}
         <div className="navbar-center hidden md:flex">
           <ul className="menu menu-horizontal text-lg px-1">
-            <li><NavLink className={""}  to={"/"}>Home</NavLink></li>
+            <li><NavLink to={"/"}>Home</NavLink></li>
             <li><NavLink to={"/about"}>About</NavLink></li>
             <li><NavLink to={"/skill"}>Skills</NavLink></li>
             <li><NavLink to={"/service"}>Services</NavLink></li>
@@ -63,7 +67,6 @@ const Header = () => {
         </div>
       </div>
     </div>
-
   );
 };
 
