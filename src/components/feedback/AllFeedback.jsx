ec07da53e2d2d7ed6c3a80f7c97b7542fecc865b
ishanrana363 <ishanrana363@gmail.com>
2024-10-12 22:39:33 +0600

    show all feedback

--- src/components/feedback/AllFeedback.jsx
+++ src/components/feedback/AllFeedback.jsx
@@ -34,23 +34,27 @@ const AllFeedback = () => {
                 slidesPerView={1}
                 pagination={{ clickable: true }}
                 navigation
-                autoplay={{ delay: 3000 }}
+                speed={1000}
+                autoplay={{ delay: 1000 }}
                 modules={[Pagination, Navigation, Autoplay]}
                 className="w-full relative"
             >
                 {feedbackList.map((fb) => (
-                    <SwiperSlide key={fb.id} className="flex flex-col items-center p-4">
-                        <img 
+                    <SwiperSlide
+                        key={fb.id}
+                        className="flex flex-col items-center justify-center p-4 h-[300px]" // Fixed height for uniformity
+                    >
+                        <img
                             src={fb.img}
                             alt={fb.name}
-                            className=" mb-2 mx-auto "
+                            className="mx-auto w-24 h-24 rounded-full object-cover" // Set fixed width and height
                         />
-                        <p className="font-bold text-lg">{fb.name}</p>
-                        <p className="text-gray-600 text-center">{fb.feedback}</p>
+                        <div className="my-4 text-center">
+                            <p className="font-bold text-lg">{fb.name}</p>
+                            <p className="text-gray-600">{fb.feedback}</p>
+                        </div>
                     </SwiperSlide>
                 ))}
-
-
             </Swiper>
         </div>
     );
