d2ce53e34bf9a8767bc7328356b639998e475425
ishanrana363 <ishanrana363@gmail.com>
2024-10-14 23:48:16 +0600

    skill page modify

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -1,57 +1,51 @@
 import { useEffect, useState } from 'react';
 import skillStore from '../../api-request/skillStore';
-import Loader from './../loder/Loder';
 import { Helmet } from 'react-helmet-async';
+import SkeletonLoader from '../loder/SkeletonLoader';
 
 const Skill = () => {
   const { allSkillDataApi, allSkillData } = skillStore();
-  const [loder, setLoder] = useState(false);
-  window.scrollTo(0,0)
+
   useEffect(() => {
+    window.scrollTo(0, 0);
     (async () => {
-      setLoder(true);
       await allSkillDataApi();
-      setLoder(false);
-    })();
+    })()
   }, []);
 
+  if (allSkillData === null || allSkillData.length === 0) {
+    return <SkeletonLoader />;
+  }
   return (
     <>
-    <Helmet>
-      <title> Skill Page </title>
-    </Helmet>
-      <div className='bg-bgPrimary min-h-screen text-white ' >
-        <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8'> {/* Responsive padding for different devices */}
-          <div className='py-10'>
-            <div>
-              {loder ? (
-                <Loader />
-              ) : (
-                <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6'>
-                  {allSkillData &&
-                    allSkillData.map((item, i) => {
-                      return (
-                        <div key={i} className="h-full">
-                          <div className="flex flex-col h-full bg-gray-400 shadow-xl rounded-lg transform transition hover:translate-x-4 duration-500 hover:shadow-2xl"> {/* Added rounded-lg for rounded corners */}
-                            <figure className="px-4 pt-4 md:px-6 md:pt-6 lg:px-10 lg:pt-10"> {/* Adjust padding for all devices */}
-                              <img
-                                src={item?.img}
-                                className="rounded-full border border-bgPrimary mx-auto"  // Added mx-auto to center image
-                                alt="Skill Image"
-                                style={{ width: '120px', height: '120px', objectFit: 'cover' }}  // Adjust size for responsiveness
-                              />
-                            </figure>
-                            <div className="flex-grow card-body items-center text-center">
-                              <h2 className="card-title text-lg md:text-xl font-mono lg:text-2xl"> {/* Responsive font sizes */}
-                                {item?.name}
-                              </h2>
-                            </div>
-                          </div>
-                        </div>
-                      );
-                    })}
+      <Helmet>
+        <title>Skill Page</title>
+      </Helmet>
+      <div className="bg-bgPrimary min-h-screen text-white">
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          {/* Responsive padding */}
+          <div className="py-10">
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
+              {allSkillData.map((item, i) => (
+                <div key={i} className="h-full">
+                  <div className="flex flex-col h-full bg-gray-400 shadow-xl rounded-lg transform transition hover:translate-x-4 duration-500 hover:shadow-2xl">
+                    {/* Card content */}
+                    <figure className="px-4 pt-4 md:px-6 md:pt-6 lg:px-10 lg:pt-10">
+                      <img
+                        src={item?.img}
+                        className="rounded-full border border-bgPrimary mx-auto"
+                        alt="Skill Image"
+                        style={{ width: '120px', height: '120px', objectFit: 'cover' }}
+                      />
+                    </figure>
+                    <div className="flex-grow card-body items-center text-center">
+                      <h2 className="card-title text-lg md:text-xl font-mono lg:text-2xl">
+                        {item?.name}
+                      </h2>
+                    </div>
+                  </div>
                 </div>
-              )}
+              ))}
             </div>
           </div>
         </div>
