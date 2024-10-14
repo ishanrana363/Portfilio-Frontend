2ed6ff27f3797f6d25ffb398f46990f225d7758e
ishanrana363 <ishanrana363@gmail.com>
2024-10-14 23:52:02 +0600

    project page updated

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,59 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import projectStore from "../../api-request/projectStore";
-import Loader from "../loder/Loder";
 import { Link } from "react-router-dom";
+import SkeletonLoader from "../loder/SkeletonLoader";
 
 const Project = () => {
   const { projectDataListApi, projectDataList } = projectStore();
-  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Ensure the page scrolls to the top on mount.
-    const fetchData = async () => {
-      setLoader(true);
-      await projectDataListApi();
-      setLoader(false);
-    };
-    fetchData();
+    window.scrollTo(0, 0); // Ensure page scrolls to the top on load
+    (async () => {
+      await projectDataListApi(); // Fetch project data
+    })();
   }, []);
 
+  // Display loader if data is not available
+  if (projectDataList === null || projectDataList.length === 0) {
+    return <SkeletonLoader />;
+  }
+
+  // Render the project data
   return (
     <div className="bg-bgPrimary text-white min-h-screen">
       <div className="max-w-screen-xl mx-auto">
         <h1 className="text-center font-serif text-2xl py-8 text-textSecondary">
           All Projects
         </h1>
-
-        {loader ? (
-          <Loader />
-        ) : (
-          <div className="grid gap-5 mx-auto grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-10">
-            {projectDataList && projectDataList.length > 0 ? (
-              projectDataList.map((item, i) => (
-                <div key={i} className="flex justify-center">
-                  <div className="card bg-gray-400 shadow-xl transition duration-500 hover:translate-x-3 hover:shadow-2xl flex flex-col justify-between min-h-full">
-                    <Link target="_blank" to={item.url} className="h-full">
-                      <figure className="px-10 pt-10">
-                        <img
-                          src={item.img}
-                          alt={item?.name || "Project Image"}
-                          className="rounded-xl w-20 h-20 md:w-48 md:h-48 object-cover"
-                        />
-                      </figure>
-                      <div className="card-body text-center flex-grow flex flex-col justify-end pb-2">
-                        <h2 className="card-title">{item?.name || "Project Title"}</h2>
-                      </div>
-                    </Link>
+        <div className="grid gap-5 mx-auto grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-10">
+          {projectDataList.map((item, i) => (
+            <div key={i} className="flex justify-center">
+              <div className="card bg-gray-400 shadow-xl transition duration-500 hover:translate-x-3 hover:shadow-2xl flex flex-col justify-between min-h-full">
+                <Link target="_blank" to={item.url} className="h-full">
+                  <figure className="px-10 pt-10">
+                    <img
+                      src={item.img}
+                      alt={item?.name || "Project Image"}
+                      className="rounded-xl w-20 h-20 md:w-48 md:h-48 object-cover"
+                    />
+                  </figure>
+                  <div className="card-body text-center flex-grow flex flex-col justify-end pb-2">
+                    <h2 className="card-title">
+                      {item?.name || "Project Title"}
+                    </h2>
                   </div>
-                </div>
-              ))
-            ) : (
-              <div className="text-center col-span-full text-lg">
-                No projects available.
+                </Link>
               </div>
-            )}
-          </div>
-        )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
