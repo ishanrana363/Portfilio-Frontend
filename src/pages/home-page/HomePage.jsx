9fe8949a166a5f852761f90b852c4af4117d7340
ishanrana363 <ishanrana363@gmail.com>
2024-10-14 08:59:33 +0600

    home page added projec page added

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -3,14 +3,15 @@ import ProjectForm from "../../components/have-project/ProjectForm"
 import Home from "../../components/home/Home"
 import Skill from './../../components/skill/Skill';
 import FeedbackPage from "../feedback-page/FeedbackPage";
+import ProjectPage from './../project-page/ProjectPage';
 
 
 const HomePage = () => {
   return (
     <>
-    <Helmet>
-      <title>Home Page</title>
-    </Helmet>
+      <Helmet>
+        <title>Home Page</title>
+      </Helmet>
       <div>
         <div className="bg-bgPrimary" >
           <Home></Home>
@@ -18,10 +19,13 @@ const HomePage = () => {
         </div>
       </div>
       <div className="bg-[#54C392] py-8 font-mono " >
-      <ProjectForm></ProjectForm>
+        <ProjectForm></ProjectForm>
       </div>
       <div>
         <Skill></Skill>
+      </div>
+      <div>
+        <ProjectPage></ProjectPage>
       </div>
       <div>
         <FeedbackPage></FeedbackPage>
