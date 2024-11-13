a3785dbdded0e7b823a699af99790ac37d2c9bda
ishanrana363 <ishanrana363@gmail.com>
2024-11-13 21:24:34 +0600

    about page responsive design completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,11 @@
 const About = () => {
   window.scrollTo(0, 0);
   return (
-    <div className="bg-bgPrimary">
+    <div className="bg-gradient-to-br from-teal-400 via-sky-300 via-blue-200 to-purple-500 bg-opacity-100">
       <div className="max-w-screen-xl mx-auto min-h-[calc(100vh-318px)] flex items-center p-4 ">
-        <p 
-          className="text-white lg:text-lgP md:text-mdP text-smP  text-center font-mono "
-         // Animation duration and easing
+        <p
+          className="text-gray-900  md:text-2xl text-[10px]  text-center font-mono "
+        // Animation duration and easing
         >
           My name is Ishan Rana. I am from Biral, Dinajpur. I passed my SSC in 2020. Currently, I am studying in the Computer Department at Thakurgaon Polytechnic Institute. I am also doing an internship at Universe Soft Tech, focusing on the MERN stack. Alongside my internship, I am completing a course in MERN development with a mentor. I have been studying in the Computer Department for the past four years. During this time, I have developed a strong foundation in various computer science concepts and practices. I am passionate about coding and constantly strive to enhance my skills and knowledge in this ever-evolving field.
         </p>
